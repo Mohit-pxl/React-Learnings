4cd8ad751d31f3fee3a6b6dcae05fea435804022
Mohit <mohitydv1979@gmail.com>
2026-05-25 13:04:54 +0530

    food ordering app setup

---
+++ FoodOrderningApp/app.js
@@ -0,0 +1,400 @@
+import React from "react";
+import {createRoot} from "react-dom/client";
+
+
+
+const resList = [
+{
+"info": {
+"id": "150591",
+"name": "Satkar Restaurant",
+"cloudinaryImageId": "rvxp5xbniat84r6efku2",
+"locality": "Sinchai Colony",
+"areaName": "Satkar Chowk",
+"costForTwo": "₹250 for two",
+"cuisines": [
+"South Indian",
+"Indian",
+"Salads",
+"Desserts"
+],
+"avgRating": 4.4,
+"veg": true,
+"parentId": "21553",
+"avgRatingString": "4.4",
+"totalRatingsString": "3.6K+",
+"sla": {
+"deliveryTime": 64,
+"lastMileTravel": 13.4,
+"serviceability": "SERVICEABLE",
+"slaString": "60-70 mins",
+"lastMileTravelString": "13.4 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2026-05-25 11:00:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "60% OFF",
+"subHeader": "UPTO ₹110"
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-445eb039-92ae-4e62-a8b8-282ab0db4287"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/chhindwara/satkar-restaurant-sinchai-colony-satkar-chowk-rest150591",
+"type": "WEBLINK"
+}
+},
+{
+"info": {
+"id": "658210",
+"name": "The Fusion Lounge",
+"cloudinaryImageId": "fa4944f0cfdcbca2bec1f3ab8e3db3f7",
+"locality": "Triloki nagar",
+"areaName": "Railway Station",
+"costForTwo": "₹300 for two",
+"cuisines": [
+"South Indian",
+"Chinese",
+"Beverages",
+"Fast Food",
+"Desserts"
+],
+"avgRating": 4.1,
+"parentId": "395453",
+"avgRatingString": "4.1",
+"totalRatingsString": "383",
+"sla": {
+"deliveryTime": 74,
+"lastMileTravel": 12.4,
+"serviceability": "SERVICEABLE",
+"slaString": "70-80 mins",
+"lastMileTravelString": "12.4 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2026-05-25 23:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "v1695133679/badges/Pure_Veg111.png",
+"description": "pureveg"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "pureveg",
+"imageId": "v1695133679/badges/Pure_Veg111.png",
+"theme": ""
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "10% OFF",
+"subHeader": "UPTO ₹40"
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-445eb039-92ae-4e62-a8b8-282ab0db4287"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/chhindwara/the-fusion-lounge-triloki-nagar-railway-station-rest658210",
+"type": "WEBLINK"
+}
+},
+{
+"info": {
+"id": "151515",
+"name": "Gupta Bhojnalay",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2026/4/11/9855a759-3695-46c5-9a92-2ef55f67b57f_151515.jpg",
+"locality": "fulwara chowk",
+"areaName": "Chhindwara Locality",
+"costForTwo": "₹200 for two",
+"cuisines": [
+"Fast Food",
+"Indian",
+"Beverages"
+],
+"avgRating": 3.8,
+"veg": true,
+"parentId": "91635",
+"avgRatingString": "3.8",
+"totalRatingsString": "472",
+"sla": {
+"deliveryTime": 68,
+"lastMileTravel": 13.5,
+"serviceability": "SERVICEABLE",
+"slaString": "65-75 mins",
+"lastMileTravelString": "13.5 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2026-05-25 11:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "v1695133679/badges/Pure_Veg111.png",
+"description": "pureveg"
+}
+]
+},
+"isOpen": true,
+"aggregatedDiscountInfoV2": {},
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "pureveg",
+"imageId": "v1695133679/badges/Pure_Veg111.png",
+"theme": ""
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-445eb039-92ae-4e62-a8b8-282ab0db4287"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/chhindwara/gupta-bhojnalay-fulwara-chowk-chhindwara-locality-rest151515",
+"type": "WEBLINK"
+}
+},
+{
+"info": {
+"id": "522144",
+"name": "Blue Chilli Restaurant",
+"cloudinaryImageId": "je6qlse8csxgcfpuoybe",
+"locality": "Ambedkar Nagar",
+"areaName": "Prasia Road",
+"costForTwo": "₹150 for two",
+"cuisines": [
+"South Indian",
+"Indian",
+"Fast Food"
+],
+"avgRating": 3.9,
+"veg": true,
+"parentId": "311804",
+"avgRatingString": "3.9",
+"totalRatingsString": "235",
+"sla": {
+"deliveryTime": 63,
+"lastMileTravel": 10.9,
+"serviceability": "SERVICEABLE",
+"slaString": "60-70 mins",
+"lastMileTravelString": "10.9 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2026-05-25 11:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "v1695133679/badges/Pure_Veg111.png",
+"description": "pureveg"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "pureveg",
+"imageId": "v1695133679/badges/Pure_Veg111.png",
+"theme": ""
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹60"
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-445eb039-92ae-4e62-a8b8-282ab0db4287"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/chhindwara/blue-chilli-restaurant-ambedkar-nagar-prasia-road-rest522144",
+"type": "WEBLINK"
+}
+}
+]
+
+
+const Header=()=>{
+    return(
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src={"https://cdn.dribbble.com/userupload/4288549/file/original-438b97178ade9d02cb34fd941adc6357.jpg?resize=752x564&vertical=center"}/>
+                </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+
+
+
+
+
+const ResCard=(props)=>{
+    const {resData}=props;
+    const {id,cloudinaryImageId,name,cuisines,costForTwo,areaName}=resData?.info
+    return(
+        <div className="res-card">
+            <img className="res-logo" alt="res-logo" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +cloudinaryImageId }></img>
+            <h3>{name}</h3>
+            <h4>{cuisines.join(", ")}</h4>
+            <h5>{costForTwo}</h5>
+            <h5>{areaName}</h5>
+        </div>
+    )
+
+}
+
+const Body=()=>{
+    return(<div className="body">
+        <div className="search-bar">
+            search
+        </div>
+        <div className="res-container">
+            {
+                resList.map((restaurant)=>(
+                <ResCard key={restaurant.info.id} resData={restaurant}/>
+                ))
+            }
+        </div>
+    </div>
+    )
+}
+
+const AppLayout=()=>{
+    return(
+        <div className="app">
+            <Header/>
+            <Body/>
+        </div>
+    )
+}
+
+const root=createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);
